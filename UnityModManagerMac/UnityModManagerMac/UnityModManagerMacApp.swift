d120c2f7e8c7333e69e8d1383c202f3d4d0904a2
koren <131921956+kkorenn@users.noreply.github.com>
2026-05-29 23:19:27 +0900

    empty swift app

---
+++ UnityModManagerMac/UnityModManagerMac/UnityModManagerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnityModManagerMacApp.swift
+//  UnityModManagerMac
+//
+//  Created by Koren on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct UnityModManagerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnityModManagerMac/UnityModManagerMacTests/UnityModManagerMacTests.swift
@@ -0,0 +1,19 @@
+//
+//  UnityModManagerMacTests.swift
+//  UnityModManagerMacTests
+//
+//  Created by Koren on 5/29/26.
+//
+
+import Testing
+@testable import UnityModManagerMac
+
+struct UnityModManagerMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
